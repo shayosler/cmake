d0021fa7319d6d81b0f67430e18a5266125a04c2
Shay <iwishiwuzskiing@gmail.com>
2022-09-16 21:02:22 -0400

    updated build scripts to not use cmake

--- build_package.sh
+++ build_package.sh
@@ -1,17 +1,57 @@
-#!/bin/bash -x
-# build a debian package of gss build packages
+#!/bin/bash
+# Build a .deb package.
+# Files to be included in the package should be in a directory
+# with the same name as the package to build
+
+# Build parameters:
+pkgname=so-build
+description="SNO cmake files"
 version=$(cmake -DVERSION_DIR="./" -P ./get_version.cmake 2>&1)
-echo "Making SO CMake package"
-checkinstall --nodoc \
---fstrans=yes \
---maintainer='support@greenseainc.com' \
---pkgname=so-build \
---pkglicense= \
---pkggroup=SO \
---pkgversion=$version \
---arch=all \
---provides=so-build \
---backup=no \
--y \
---install=no \
-./install_so_build.sh
+arch=all
+pkgversion=1
+depends=""
+conflicts=
+replaces=
+breaks=
+run_ldconfig=false
+
+# Clean previous build
+mkdir -p build
+rm -rf build/*
+
+# Build package
+deb_name="${pkgname}_${version}-${pkgversion}_${arch}"
+deb_dir="build/$deb_name"
+mkdir -p "${deb_dir}/usr/local/bin/" || exit 1
+mkdir -p "${deb_dir}/usr/local/share/cmake/" || exit 1
+mkdir -p "${deb_dir}/usr/local/share/gss/" || exit 1
+mkdir -p "${deb_dir}/usr/local/src/" || exit 1
+current=$(pwd)
+./install_so_build.sh "$(pwd)/${deb_dir}"
+cd "$current" || { echo "Failed to return to base directory"; exit 1; }
+mkdir "$deb_dir/DEBIAN/"
+cat << EOF > "$deb_dir/DEBIAN/control"
+Package: $pkgname
+Version: $version-$pkgversion
+Section: SO
+Priority: optional
+Architecture: $arch
+Depends: $depends
+Breaks: $breaks
+Replaces: $replaces
+Conflicts: $conflicts
+Maintainer: Greensea Systems <support@greensea.com>
+Description: $description
+EOF
+
+# Trigger ldconfig if necessary
+if [[ "$run_ldconfig" == "true" ]]
+then
+  cat << EOF > "$deb_dir/DEBIAN/triggers"
+activate-noawait ldconfig
+EOF
+fi
+
+fakeroot dpkg-deb --build "$deb_dir"
+
+cp "build/${deb_name}.deb" ./
--- install_so_build.sh
+++ install_so_build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cmake_dir=/usr/local/share/cmake/so_cmake
-bin_dir=/usr/local/bin/
+root="$1"
+cmake_dir="$root"/usr/local/share/cmake/so_cmake
+bin_dir="$root"/usr/local/bin/
 
 [ -d /usr/local/share/cmake ] || mkdir /usr/local/share/cmake/
-[ -d $cmake_dir ] || mkdir $cmake_dir
-cp *.cmake $cmake_dir
-cp *.in $cmake_dir
-cp -rf doc $cmake_dir
-cp -rf cmake-modules $cmake_dir
-cp get_version.sh $bin_dir
+[ -d "$cmake_dir" ] || mkdir "$cmake_dir"
+cp *.cmake "$cmake_dir"
+cp *.in "$cmake_dir"
+cp -rf doc "$cmake_dir"
+cp -rf cmake-modules "$cmake_dir"
+cp get_version.sh "$bin_dir"
